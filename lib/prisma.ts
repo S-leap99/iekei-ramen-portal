b28c0449e830232d5fee84d4f2f971ef154bd9e2
S-leap99 <donai8nen.8don@gmail.com>
2025-07-26 16:39:51 +0900

    Supabase 切り替え反映

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,15 @@
+// lib/prisma.js
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+const globalForPrisma = globalThis
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: ['error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  }
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
